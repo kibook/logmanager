0c23ffd9d9717e15a39002f085edd00d87bf10e7
kibook <kibuzhj@gmail.com>
2021-08-03 11:53:25 -0400

    Default to invoking resource name
    If no resource name is specified, use GetInvokingResource to get the name of the resource invoking the export to use as the default

--- server.lua
+++ server.lua
@@ -13,7 +13,7 @@ local function log(entry)
 	end
 
 	if not entry.resource then
-		entry.resource = GetCurrentResourceName()
+		entry.resource = GetInvokingResource() or GetCurrentResourceName()
 	end
 
 	if entry.player then
